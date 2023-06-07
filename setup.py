5926b863cee53c2763c2debc74946a686ac4a8b6
Jody Phelan <jody.phelan@lshtm.ac.uk>
2023-06-07 16:05:56 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from glob import glob
 version = [l.strip() for l in open("dengue_ngs/__init__.py") if "version" in l][0].split('"')[1]
 
 setuptools.setup(
-	name="dengue_wgs",
+	name="dengue_ngs",
 	version=version,
-	packages=["dengue_wgs"],
+	packages=["dengue_ngs"],
 	license="GPLv3",
 	long_description="Dengue wgs command line tool",
 	scripts= glob("scripts/*"),
