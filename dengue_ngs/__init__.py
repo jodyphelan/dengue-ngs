918e79bfb4fa3f45452b21ef2fb169d4f35a73ff
Jody Phelan <jody.phelan@lshtm.ac.uk>
2023-09-27 12:31:34 +0000

    bump version

--- dengue_ngs/__init__.py
+++ dengue_ngs/__init__.py
@@ -14,7 +14,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import re
 
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 
 def get_strand_direction(consensus,ref):
     """
